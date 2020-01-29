f5c30f833f37dc181fbda802941b0291afddda0e
steevehook <steevehook@gmail.com>
2020-01-30 01:28:01 +0200

    refactor a bit insertion sort example

--- sorting/insertion/main.go
+++ sorting/insertion/main.go
@@ -4,17 +4,15 @@ import "fmt"
 
 func main() {
 	sorted := insertionSort([]int{5, 2, 4, 6, 1, 3})
-	fmt.Println(sorted)
+	fmt.Println(sorted) // [1,2,3,4,5,6]
 }
 
 func insertionSort(numbers []int) []int {
 	A := numbers[:]
 	for j := 1; j < len(A); j++ {
-		key := A[j]
-		i := j - 1
+		key, i := A[j], j-1
 		for i > -1 && A[i] > key {
-			A[i+1] = A[i]
-			i -= 1
+			A[i+1], i = A[i], i-1
 		}
 		A[i+1] = key
 	}
